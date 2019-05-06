e52604efc36dd96bc5c55d4ab9e1e31d1348fc2f
MJ Abadilla <mjmaix@gmail.com>
2019-05-06 18:11:36 +0800

    Tests for Reverse Integer

--- 7.reverse-integer.test.ts
+++ 7.reverse-integer.test.ts
@@ -1,26 +1,26 @@
 import assert from "assert";
 import { reverse } from "./7.reverse-integer";
 
-describe("Reverse Integer", function() {
-  it("Input: 123, Output: 321", function() {
+describe("Reverse Integer", () => {
+  it("Input: 123, Output: 321", () => {
     const expected = 321;
     const input = 123;
     const rev = reverse(input);
     assert.equal(rev, expected);
   });
-  it("Input: -123, Output: -321", function() {
+  it("Input: -123, Output: -321", () => {
     const expected = -321;
     const input = -123;
     const rev = reverse(input);
     assert.equal(rev, expected);
   });
-  it("Input: 120, Output: 21", function() {
+  it("Input: 120, Output: 21", () => {
     const expected = 21;
     const input = 120;
     const rev = reverse(input);
     assert.equal(rev, expected);
   });
-  it("Input: 99999999999, Output: 0", function() {
+  it("Input: 99999999999, Output: 0", () => {
     const expected = 0;
     const input = 99999999999;
     const rev = reverse(input);
